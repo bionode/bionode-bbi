1d59d0a2987c4aab8d251d2184a05b91726376ff
Thomas Down <thomas.a.down@gmail.com>
2014-07-31 22:41:10 +0100

    Added node-side IO wrapper from @bmpvieira

---
+++ README.md
@@ -0,0 +1,5 @@
+Fetcher for BBI files (bigWig and bigBed).
+
+Based on code from [Biodalliance](http://www.biodalliance.org/)
+
+Still needs some node-side tests etc.
---
+++ src/node-fetchable.js
@@ -0,0 +1,47 @@
+// from @bmpvieira
+
+"use strict";
+
+// Imports
+var fs = require('fs');
+var concat = require('concat-stream');
+
+var Promise = require('es6-promise').Promise;
+
+function NodeFetchable(blob, opts) {
+    this.blob = blob;
+    this.opts = opts;
+}
+
+NodeFetchable.prototype.slice = function(start, length) {
+    // Both start and end are inclusive and start at 0 (http://nodejs.org/api/fs.html)
+    var opts = {start: start};
+    if (length) { opts.end = start + length };
+    return new NodeFetchable(this.blob, opts);
+}
+
+NodeFetchable.prototype.fetch = function() {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+        var read = fs.createReadStream(self.blob, self.opts);
+        var write = concat(function(data) {
+            resolve(toArrayBuffer(data));
+        });
+        read.pipe(write);
+    });
+}
+
+function toArrayBuffer(buffer) {
+    // Node 0.12+ has a built-in buffer.toArrayBuffer() method that will deprecate this
+    var ab = new ArrayBuffer(buffer.length);
+    var view = new Uint8Array(ab);
+    for (var i = 0; i < buffer.length; ++i) {
+        view[i] = buffer[i];
+    }
+    return ab;
+}
+
+module.exports = {
+    NodeFetchable: NodeFetchable
+}
+
